85675604710183abf94009a6ed7be52192615653
Admin <admin@Admins-Mac.local>
2017-03-09 13:29:05 +0530

    Adding the view Templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,14 @@
 
 
-module.exports.homeList   = function(req, res, next) {
-  res.render('index', { title: 'Express' });
+module.exports.homeList   = function(req, res) {
+  res.render('locations-list', { title: 'Home' });
 };
+
+module.exports.locationInfo = function(req, res) {
+	res.render('location-info', {title: 'Location Info'});
+};
+
+module.exports.addReview = function(req, res) {
+	res.render('location-review-form', {title: 'Add Review'});
+};
+
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var ctrlLocations   = require('../controllers/locations');
+var ctrlOthers 		= require('../controllers/others');
 
+// Define locations routes and mapping them to controllers functions
 /* GET home page. */
 router.get('/', ctrlLocations.homeList);
+
+/* Get 'Location Info' page */
+router.get('/location', ctrlLocations.locationInfo);
+
+/* Get 'Add Review' page */
+router.get('/location/review/new', ctrlLocations.addReview);
+
+
+// Define others routes and mapping them to controllers functions
+/* Get 'About' page */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
